1452a034686d853725070741e72ca57c12690f11
ashishsingh30 <ashishksingh3004@gmail.com>
2017-07-15 17:07:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var Employee = {name:"Ashish", age:"23", Salary:"333000", 
+Address:{City:"Gandhinagar", State:"Gujarat", Pincode:"382016"}
+               };
+console.log("Name : "+Employee.name);
+console.log("Age : "+Employee.age);
+console.log("Salary : "+Employee.Salary);
+console.log("City : "+Employee.Address.City);
+console.log("State : "+Employee.Address.State);
+console.log("Pincode : "+Employee.Address.Pincode);
